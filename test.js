4a356121656c8414835b2b62f7abf6ecc2fcb030
Igor Kroitor <igor.kroitor@gmail.com>
2017-07-21 02:00:56 +0300

    added test.js

---
+++ test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var asciichart = require ('./asciichart')
+
+var width = 120
+var line = "\n" + '='.repeat (width + 9) + "\n"
+
+console.log ("\nbasic\n")
+
+var s0 = new Array (width)
+for (var i = 0; i < s0.length; i++)
+    s0[i] = 15 * Math.sin (i * ((Math.PI * 4) / s0.length))
+console.log (asciichart.plot (s0))
+
+console.log (line)
+console.log ("configuring / scale to desired height\n")
+
+var config = {
+    offset:  3,          // axis offset from the left (min 2)
+    padding: '       ',  // padding string for label formatting (can be overrided)
+    height:  10,         // any height you want
+}
+
+var s = []
+for (var i = 0; i < width; i++)
+    s[i] = 15 * Math.cos (i * ((Math.PI * 8) / width)) // values range from -15 to +15
+console.log (asciichart.plot (s, config))     // this rescales the graph to ±3 lines
+
+console.log (line)
+console.log ("auto-range\n")
+
+var s2 = new Array (width)
+s2[0] = Math.round (Math.random () * 15)
+for (i = 1; i < s2.length; i++)
+    s2[i] = s2[i - 1] + Math.round (Math.random () * (Math.random () > 0.5 ? 2 : -2))
+console.log (asciichart.plot (s2) + "\n")
