0b7a325e4cd5c951f0ea0a6fe74d59aca9d91392
Igor Kroitor <igor.kroitor@gmail.com>
2017-07-22 01:43:58 +0300

    fixing for scrutinizer-ci

--- asciichart.js
+++ asciichart.js
@@ -4,55 +4,56 @@
 
     exports.plot = function (series, cfg) {
 
-        var min = series[0]
-        var max = series[0]
+        let min = series[0]
+        let max = series[0]
 
-        for (var i = 1; i < series.length; i++) {
+        for (let i = 1; i < series.length; i++) {
             min = Math.min (min, series[i])
             max = Math.max (max, series[i])
         }
 
-        var range   = Math.abs (max - min)
-        var cfg     = (typeof cfg !== 'undefined') ? cfg : {}
-        var offset  = (typeof cfg.offset  !== 'undefined') ? cfg.offset  : 3
-        var padding = (typeof cfg.padding !== 'undefined') ? cfg.padding : '       '
-        var height  = (typeof cfg.height  !== 'undefined') ? cfg.height  : range
-        var ratio   = height / range
-        var min2    = Math.round (min * ratio)
-        var max2    = Math.round (max * ratio)
-        var rows    = Math.abs (max2 - min2)
-        var width   = series.length + offset
-        var format  = (typeof cfg.format !== 'undefined') ? cfg.format : function (x, i) {
+        let range   = Math.abs (max - min)
+        let cfg     = (typeof cfg !== 'undefined') ? cfg : {}
+        let offset  = (typeof cfg.offset  !== 'undefined') ? cfg.offset  : 3
+        let padding = (typeof cfg.padding !== 'undefined') ? cfg.padding : '       '
+        let height  = (typeof cfg.height  !== 'undefined') ? cfg.height  : range
+        let ratio   = height / range
+        let min2    = Math.round (min * ratio)
+        let max2    = Math.round (max * ratio)
+        let rows    = Math.abs (max2 - min2)
+        let width   = series.length + offset
+        let format  = (typeof cfg.format !== 'undefined') ? cfg.format : function (x, i) {
             return (padding + x.toFixed (2)).slice (-padding.length)
         }
 
-        var result = new Array (rows + 1) // empty space
-        for (var i = 0; i <= rows; i++) {
+        let result = new Array (rows + 1) // empty space
+        for (let i = 0; i <= rows; i++) {
             result[i] = new Array (width)
-            for (var j = 0; j < width; j++)
+            for (let j = 0; j < width; j++) {
                 result[i][j] = ' '
+            }
         }
 
-        for (var y = min2; y <= max2; ++y) { // axis + labels
-            var label = format (max - (y - min2) * range / rows, y - min2)
+        for (let y = min2; y <= max2; ++y) { // axis + labels
+            let label = format (max - (y - min2) * range / rows, y - min2)
             result[y - min2][Math.max (offset - label.length, 0)] = label
             result[y - min2][offset - 1] = (y == 0) ? '┼' : '┤' 
         }
 
-        var y0 = Math.round (series[0] * ratio) - min2
+        let y0 = Math.round (series[0] * ratio) - min2
         result[rows - y0][offset - 1] = '┼' // first value
 
-        for (var x = 0; x < series.length - 1; x++) { // plot the line
-            var y0 = Math.round (series[x + 0] * ratio) - min2
-            var y1 = Math.round (series[x + 1] * ratio) - min2
-            if (y0 == y1)
+        for (let x = 0; x < series.length - 1; x++) { // plot the line
+            let y0 = Math.round (series[x + 0] * ratio) - min2
+            let y1 = Math.round (series[x + 1] * ratio) - min2
+            if (y0 == y1) {
                 result[rows - y0][x + offset] = '─'
-            else {                        
+            } else {                        
                 result[rows - y1][x + offset] = (y0 > y1) ? '╰' : '╭'
                 result[rows - y0][x + offset] = (y0 > y1) ? '╮' : '╯'
-                var from = Math.min (y0, y1)
-                var to = Math.max (y0, y1)
-                for (var y = from + 1; y < to; y++)
+                let from = Math.min (y0, y1)
+                let to = Math.max (y0, y1)
+                for (let y = from + 1; y < to; y++)
                     result[rows - y][x + offset] = '│'
             }
         }
