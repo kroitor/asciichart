78deffe2f73025b7926ddd3ae09f4544e55f5472
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-01 19:16:17 +0300

    py version

--- asciichart.py
+++ asciichart.py
@@ -5,22 +5,20 @@ from math import sin
 from math import pi
 from math import floor
 from math import ceil
-from decimal import Decimal
-
-import ccxt
 
 def plot(series, cfg={}):
 
     minimum = min(series)
     maximum = max(series)
 
-    interval = abs(maximum - minimum)
+    interval = abs(float(maximum) - float(minimum))
     offset = cfg['offset'] if 'offset' in cfg else 3
     padding = cfg['padding'] if 'padding' in cfg else '       '
     height = cfg['height'] if 'height' in cfg else interval
     ratio = height / interval
-    min2 = floor(minimum * ratio)
-    max2 = ceil(maximum * ratio)
+    # print(minimum,ratio,type(minimum))
+    min2 = floor(float(minimum) * ratio)
+    max2 = ceil(float(maximum) * ratio)
 
     intmin2 = int(min2)
     intmax2 = int(max2)
@@ -33,13 +31,14 @@ def plot(series, cfg={}):
     
     # axis and labels
     for y in range(intmin2, intmax2 + 1):
-        label = '{:8.2f} '.format (maximum - ((y - intmin2) * interval / rows))
+        label = '{:8.2f} '.format (float(maximum) - ((y - intmin2) * interval / rows))
         result[y - intmin2][max(offset - len(label), 0)] = label
         result[y - intmin2][offset - 1] = '┼' if y == 0 else '┤'
 
-    y0 = int(round(series[0] * ratio) - min2)
+    y0 = int(series[0] * ratio - min2)
     result[rows - y0][offset - 1] = '┼' # first value
 
+    print (len(series) - 1)
     for x in range(0,len(series) - 1): # plot the line
         y0 = int(round(series[x + 0] * ratio) - intmin2)
         y1 = int(round(series[x + 1] * ratio) - intmin2)
@@ -48,9 +47,9 @@ def plot(series, cfg={}):
         else:
             result[rows - y1][x + offset] = '╰' if y0 > y1 else '╭'
             result[rows - y0][x + offset] = '╮' if y0 > y1 else '╯'
-            start = min(y0, y1)
+            start = min(y0, y1) + 1
             end = max(y0, y1)
-            for y in range(start + 1, end):
+            for y in range(start, end):
                 result[rows - y][x + offset] = '│'
 
     return '\n'.join([''.join(row) for row in result])
@@ -59,10 +58,3 @@ if __name__ == '__main__':
     width = 180
     series = [15 * cos(i * ((pi * 4) / width)) for i in range(width)]
     print(plot(series))
-
-    ohlcv = ccxt.kraken().fetch_ohlcv ('BTC/USD', 900)
-    last = ohlcv[len(ohlcv) - 1][index] # closing price
-    series = [x[index] for x in ohlcv]
-    rate = '₿ = $' + str(last)
-    print(plot (series, { 'height': 15 }))
-    
