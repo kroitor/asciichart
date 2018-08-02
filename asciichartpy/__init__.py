9fd61681e402ff80b9120697b70131e6ee9fb5ad
zhustec <zhustec@foxmail.com>
2018-08-02 14:52:08 +0800

    removing unnecessary variables

--- asciichartpy/__init__.py
+++ asciichartpy/__init__.py
@@ -24,30 +24,27 @@ def plot(series, cfg={}):
     padding = cfg['padding'] if 'padding' in cfg else '           '
     height = cfg['height'] if 'height' in cfg else interval
     ratio = height / interval
-    min2 = floor(float(minimum) * ratio)
-    max2 = ceil(float(maximum) * ratio)
+    intmin = floor(float(minimum) * ratio)
+    intmax = ceil(float(maximum) * ratio)
 
-    intmin2 = int(min2)
-    intmax2 = int(max2)
-
-    rows = abs(intmax2 - intmin2)
+    rows = abs(intmax - intmin)
     width = len(series) + offset
     placeholder = cfg['format'] if 'format' in cfg else '{:8.2f} '
 
     result = [[' '] * width for i in range(rows + 1)]
 
     # axis and labels
-    for y in range(intmin2, intmax2 + 1):
-        label = placeholder.format(float(maximum) - ((y - intmin2) * interval / rows))
-        result[y - intmin2][max(offset - len(label), 0)] = label
-        result[y - intmin2][offset - 1] = '┼' if y == 0 else '┤'
+    for y in range(intmin, intmax + 1):
+        label = placeholder.format(float(maximum) - ((y - intmin) * interval / rows))
+        result[y - intmin][max(offset - len(label), 0)] = label
+        result[y - intmin][offset - 1] = '┼' if y == 0 else '┤'
 
-    y0 = int(series[0] * ratio - min2)
+    y0 = int(series[0] * ratio - intmin)
     result[rows - y0][offset - 1] = '┼' # first value
 
     for x in range(0, len(series) - 1): # plot the line
-        y0 = int(round(series[x + 0] * ratio) - intmin2)
-        y1 = int(round(series[x + 1] * ratio) - intmin2)
+        y0 = int(round(series[x + 0] * ratio) - intmin)
+        y1 = int(round(series[x + 1] * ratio) - intmin)
         if y0 == y1:
             result[rows - y0][x + offset] = '─'
         else:
