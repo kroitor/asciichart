e0bb221f173bb70f3a1f84051a4c5a21f4284e0c
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-26 08:17:55 +0300

    test.py edits

--- test.py
+++ test.py
@@ -13,4 +13,10 @@ from asciichartpy import plot
 
 width = 90
 series = [7 * round(cos(i * ((pi * 4) / width)), 2) for i in range(width)]
+
 print(plot(series))
+
+series = [2.0]*width
+
+print(plot(series))
+print(plot(series, {'height':5}))
