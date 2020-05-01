11ce0f271952ed5ad154325c104cdf48ec2baa81
Christian Clauss <cclauss@me.com>
2020-05-01 09:40:54 +0200

    Adjust format

--- asciichartpy/__init__.py
+++ asciichartpy/__init__.py
@@ -69,12 +69,12 @@ def plot(series, cfg=None):
     decimal point:
 
         >>> series = [10,20,30,40,50,40,30,20,10]
-        >>> print(plot(series, {'height': 4, 'format':'  {:8.0f}'}))
-            50 ┤   ╭╮
-            40 ┤  ╭╯╰╮
-            30 ┤ ╭╯  ╰╮
-            20 ┤╭╯    ╰╮
-            10 ┼╯      ╰
+        >>> print(plot(series, {'height': 4, 'format':'{:8.0f}'}))
+          50 ┤   ╭╮
+          40 ┤  ╭╯╰╮
+          30 ┤ ╭╯  ╰╮
+          20 ┤╭╯    ╰╮
+          10 ┼╯      ╰
 	"""
     if len(series) == 0 or all(isnan(n) for n in series):
         return ''
