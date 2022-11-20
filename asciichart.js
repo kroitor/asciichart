e9e76ebc6b49230a03cbc5a0afb65adbbc2e93bb
culi🌸 <culitif@tuta.io>
2022-11-20 14:06:14 -0800

    cleans up js logic

--- asciichart.js
+++ asciichart.js
@@ -25,7 +25,7 @@
 
     function colored (char, color) {
         // do not color it if color is not specified
-        return (color === undefined) ? char : (color + char + exports.reset)
+        return color ? `${color}${char || ''}${exports.reset}` : char
     }
 
     exports.colored = colored
@@ -38,59 +38,51 @@
             series = [series]
         }
 
-        cfg = (typeof cfg !== 'undefined') ? cfg : {}
+    const plot = function (series, cfg = {}) {
+        if (typeof(series[0]) == "number") series = [series]
 
-        let min = (typeof cfg.min !== 'undefined') ? cfg.min : series[0][0]
-        let max = (typeof cfg.max !== 'undefined') ? cfg.max : series[0][0]
+        const defaultSymbols = [ '┼', '┤', '╶', '╴', '─', '╰', '╭', '╮', '╯', '│' ]
+        const defaultPadding = '           '
 
-        for (let j = 0; j < series.length; j++) {
-            for (let i = 0; i < series[j].length; i++) {
-                min = Math.min(min, series[j][i])
-                max = Math.max(max, series[j][i])
-            }
-        }
+        let { 
+            min       = Math.min(...series.flat()), 
+            max       = Math.max(...series.flat()),
+            offset    = 3,
+            padding   = defaultPadding,
+            height,
+            colors    = [],
+            symbols   = defaultSymbols,
+            format
+        } = cfg;
+        const range   = Math.abs(max - min)
+        height        = height ?? range
+        format        = format ?? (x => (padding + x.toFixed(2)).slice(-padding.length))
+        const ratio   = range ? height / range : 1
+        const min2    = Math.round(min * ratio)
+        const max2    = Math.round(max * ratio)
+        const rows    = Math.abs(max2 - min2)
+        const width   = offset + Math.max(0, ...series.map(s => s.length))
 
-        let defaultSymbols = [ '┼', '┤', '╶', '╴', '─', '╰', '╭', '╮', '╯', '│' ]
-        let range   = Math.abs (max - min)
-        let offset  = (typeof cfg.offset  !== 'undefined') ? cfg.offset  : 3
-        let padding = (typeof cfg.padding !== 'undefined') ? cfg.padding : '           '
-        let height  = (typeof cfg.height  !== 'undefined') ? cfg.height  : range
-        let colors  = (typeof cfg.colors !== 'undefined') ? cfg.colors : []
-        let ratio   = range !== 0 ? height / range : 1;
-        let min2    = Math.round (min * ratio)
-        let max2    = Math.round (max * ratio)
-        let rows    = Math.abs (max2 - min2)
-        let width = 0
-        for (let i = 0; i < series.length; i++) {
-            width = Math.max(width, series[i].length)
-        }
-        width = width + offset
-        let symbols = (typeof cfg.symbols !== 'undefined') ? cfg.symbols : defaultSymbols
-        let format  = (typeof cfg.format !== 'undefined') ? cfg.format : function (x) {
-            return (padding + x.toFixed (2)).slice (-padding.length)
-        }
+        // instantiate result as 2d array of spaces of size (rows + 1) x width
+        let result = Array.from(new Array(rows + 1), () => new Array(width).fill(' '))
 
-        let result = new Array (rows + 1) // empty space
-        for (let i = 0; i <= rows; i++) {
-            result[i] = new Array (width)
-            for (let j = 0; j < width; j++) {
-                result[i][j] = ' '
-            }
-        }
-        for (let y = min2; y <= max2; ++y) { // axis + labels
-            let label = format (rows > 0 ? max - (y - min2) * range / rows : y, y - min2)
-            result[y - min2][Math.max (offset - label.length, 0)] = label
+        // axis + labels
+        for (let y = min2; y <= max2; ++y) {
+            let label = format(rows ? max - (y - min2) * range / rows : y, y - min2)
+            result[y - min2][Math.max(offset - label.length, 0)] = label
             result[y - min2][offset - 1] = (y == 0) ? symbols[0] : symbols[1]
         }
 
+        // plot each series
         for (let j = 0; j < series.length; j++) {
             let currentColor = colors[j % colors.length]
-            let y0 = Math.round (series[j][0] * ratio) - min2
+            let y0 = Math.round(series[j][0] * ratio) - min2
             result[rows - y0][offset - 1] = colored(symbols[0], currentColor) // first value
 
-            for (let x = 0; x < series[j].length - 1; x++) { // plot the line
-                let y0 = Math.round (series[j][x + 0] * ratio) - min2
-                let y1 = Math.round (series[j][x + 1] * ratio) - min2
+            // plot the line
+            for (let x = 0; x < series[j].length - 1; x++) {
+                let y0 = Math.round(series[j][x + 0] * ratio) - min2
+                let y1 = Math.round(series[j][x + 1] * ratio) - min2
                 if (y0 == y1) {
                     result[rows - y0][x + offset] = colored(symbols[4], currentColor)
                 } else {
@@ -104,7 +96,9 @@
                 }
             }
         }
-        return result.map (function (x) { return x.join ('') }).join ('\n')
+
+        return result.map(x => x.join('')).join('\n')
     }
 
+    exports.plot = plot
 }) (typeof exports === 'undefined' ? /* istanbul ignore next */ this['asciichart'] = {} : exports);
